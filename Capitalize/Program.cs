422fa1b70a442a71b726a2c51518ef0d3a10adb0
Ayxan23 <ayxan042223@gmail.com>
2022-11-23 17:33:54 +0400

    Capitalize

---
+++ Capitalize/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Capitalize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = "bU tasKI metHodla yAzmAliSINiz";
+            Capitalize(str);
+
+        }
+
+        static void Capitalize(string str)
+        {
+            str = str.ToLower();
+            string[] word = str.Split();
+
+            foreach (var item in word)
+            {
+                Console.Write(char.ToUpper(item[0]) + item.Substring(1) + " ");
+            }
+        }
+
+    }
+}
